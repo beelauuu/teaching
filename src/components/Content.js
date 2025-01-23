b63ab5fed3fc4f666bc6620e5e08fd328cd1c249
beelauuu <blau1@terpmail.umd.edu>
2025-01-23 12:49:47 -0500

    stuff

--- src/components/Content.js
+++ src/components/Content.js
@@ -1,47 +1,62 @@
-import React from 'react';
-import '../style/App.css';
-import TypingEffect from './TypingEffect';
+import React from "react";
+import "../style/App.css";
+import TypingEffect from "./TypingEffect";
 
 function Content({ isNightMode }) {
   const handleButtonClick = (url) => {
     // Open the specified URL in a new tab
-    window.open(url, '_blank');
+    window.open(url, "_blank");
   };
 
+  const discussions = [
+    { 
+      title: "Discussion 1", 
+      slidesUrl: "https://beelauuu.github.io/slides/docs/12725", 
+      codeUrl: "https://github.com/beelauuu/TASite/raw/main/public/discussion/2025/DiscussionOne.zip" 
+    },
+    { 
+      title: "Discussion 2", 
+      slidesUrl: "https://beelauuu.github.io/slides/docs/12925" 
+    }
+  ];
+
   return (
-    <div className={`col-lg-8 content ${isNightMode ? 'night-mode' : 'day-mode'}`}>
-      <h1 className={`mb-3 text-center ${isNightMode ? 'night-mode-text' : 'day-mode-text'}`}>
+    <div
+      className={`col-lg-8 content ${isNightMode ? "night-mode" : "day-mode"}`}
+    >
+      <h1
+        className={`mb-3 text-center ${isNightMode ? "night-mode-text" : "day-mode-text"}`}
+      >
         <TypingEffect text="Weekly Materials" />
       </h1>
 
       <div className="container text-center">
         <div className="wrapper-grid">
-          <div className={`wrapper mt-1 ${isNightMode ? 'night-mode-text' : 'day-mode-text'}`}>
-            <p style={{ display: 'inline' }} className={`week ${isNightMode ? 'night-mode-text' : 'day-mode-text'}`}>
-              Discussion 1:
-            </p>
-            <button
-              type="button"
-              onClick={() => handleButtonClick('https://beelauuu.github.io/slides/docs/12725')}
-              style={{ marginLeft: '10px' }}
-              className={`btn ${isNightMode ? 'btn-light' : 'btn-dark'}`}
-            >
-              Slides
-            </button>
-          </div>
-          {/* <div className={`wrapper mt-1 ${isNightMode ? 'night-mode-text' : 'day-mode-text'}`}>
-            <p style={{ display: 'inline' }} className={`week ${isNightMode ? 'night-mode-text' : 'day-mode-text'}`}>
-              Discussion 2:
-            </p>
-            <button
-              type="button"
-              onClick={() => handleButtonClick('https://beelauuu.github.io/slides/docs/12925')}
-              style={{ marginLeft: '10px' }}
-              className={`btn ${isNightMode ? 'btn-light' : 'btn-dark'}`}
-            >
-              Slides
-            </button>
-          </div> */}
+          {discussions.map((discussion) => (
+            <div key={discussion.id} className="wrapper mt-1">
+              <p style={{ display: 'inline' }} >{discussion.title}:</p>
+              
+              <button
+                type="button"
+                onClick={() => handleButtonClick(discussion.slidesUrl)}
+                style={{ marginLeft: "10px" }}
+                className={`btn ${isNightMode ? "btn-light" : "btn-dark"}`}
+              >
+                Slides
+              </button>
+              
+              {discussion.codeUrl && (
+                <button
+                  type="button"
+                  onClick={() => handleButtonClick(discussion.codeUrl)}
+                  style={{ marginLeft: "10px" }}
+                  className={`btn ${isNightMode ? "btn-light" : "btn-dark"}`}
+                >
+                  Code
+                </button>
+              )}
+            </div>
+          ))}
         </div>
       </div>
     </div>
