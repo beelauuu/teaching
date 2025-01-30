533cdedefabce23fd996e2d00c88c397c07d20e4
beelauuu <blau1@terpmail.umd.edu>
2025-01-30 15:48:35 -0500

    stuff

--- src/components/Content.js
+++ src/components/Content.js
@@ -17,7 +17,16 @@ function Content({ isNightMode }) {
     { 
       title: "Discussion 2", 
       slidesUrl: "https://beelauuu.github.io/slides/docs/12925" 
-    }
+    },
+    // {
+    //   title: "Discussion 3",
+    //   slidesUrl: "https://beelauuu.github.io/slides/docs/20325",
+    //   codeUrl: "https://github.com/beelauuu/TASite/raw/main/public/discussion/2025/DiscussionThree.zip"
+    // },
+    // {
+    //   title: "Discussion 4",
+    //   slidesUrl: "https://beelauuu.github.io/slides/docs/20525",
+    // }
   ];
 
   return (
